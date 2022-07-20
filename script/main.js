8c0ca86794442265b6fad7c852c65883301e15fc
frydaiii <manh.td@teko.vn>
2022-07-20 15:40:39 +0700

    vlg

--- script/main.js
+++ script/main.js
@@ -249,7 +249,7 @@ const animationTimeline = () => {
       {
         scale: 1,
         rotationY: 0,
-        color: "#ff69b4",
+        color: "#66CC80",
         ease: Expo.easeOut
       },
       0.1,
